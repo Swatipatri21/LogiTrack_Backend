ed887cf944a760ee5d62f641630cafba18eb0ccb
Swatipatri21 <swati21shree@gmail.com>
2026-04-01 12:45:12 +0530

    Set default timezone to Asia/Kolkata

--- src/main/java/com/deliverytracking/DeliverytrackingsystemprojectApplication.java
+++ src/main/java/com/deliverytracking/DeliverytrackingsystemprojectApplication.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class DeliverytrackingsystemprojectApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(DeliverytrackingsystemprojectApplication.class, args);
 	}
 
